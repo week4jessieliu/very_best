099af8218f83faa6ac95d40101858fdabf385523
Jessieliuzx <zliu20@chicagobooth.edu>
2017-11-22 04:25:05 +0000

    add cuisine option to filter and deleted extra filters

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,6 +3,7 @@ class DishesController < ApplicationController
     @q = Dish.ransack(params[:q])
     @dishes = @q.result(:distinct => true).includes(:cuisine, :bookmarks, :fans, :specialists).page(params[:page]).per(10)
     @bookmark = Bookmark.new
+    @cuisines = Cuisine.all
     render("dishes/index.html.erb")
   end
 
